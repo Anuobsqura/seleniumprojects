7d3d4db30d3db1548babaf7782ac61c00b5a8b8c
Anuobsqura <anusoc53@gmail.com>
2022-04-07 21:13:10 +0530

    Robot

--- seleniumtraining/src/ExcelRead/ExPath.java
+++
@@ -1,9 +0,0 @@
-package ExcelRead;
-
-public class ExPath {
-	
-
-		public static final String TESTPATH = "C:\\Users\\330 S\\Desktop\\Anu.xlsx";
-
-		}
-
--- seleniumtraining/src/SamplePackageGit/SampleNameGit.java
+++
@@ -1,9 +0,0 @@
-package SamplePackageGit;
-
-public class SampleNameGit {
-	public static void main(String[] args) {
-		System.out.println("Hai Gibi");
-	}
-}
- 
-
