25532f440912c03ed76467d77dbadd0505fc4830
Anuobsqura <anusoc53@gmail.com>
2022-04-09 08:32:51 +0530

    Alert and iframe Successs

---
+++ seleniumtraining/src/SeleniumPrograms/SeleniumAlert.java
@@ -0,0 +1,21 @@
+package SeleniumPrograms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumAlert {
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt");
+		Thread.sleep(3000);
+		driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
+		driver.findElement(By.xpath("//button[@onclick='myFunction()']")).click();
+		String alertmsg = driver.switchTo().alert().getText();
+		System.out.println(alertmsg);
+		driver.switchTo().alert().accept();
+		}
+}
